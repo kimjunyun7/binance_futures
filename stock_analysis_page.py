f70f63fefc09ff2b26306e178d08b5d2f819107c
kimjunyun7 <kimjunyun7@gmail.com>
2025-08-23 03:52:28 +0000

    edit calling render_info_section() issue

--- stock_analysis_page.py
+++ stock_analysis_page.py
@@ -29,7 +29,7 @@ def render_stock_analysis_page():
             )
 
             if selected_section == "정보":
-                render_info_section(stock, info, summary, ticker_input)
+                render_info_section(stock, info, summary)
 
             elif selected_section == "그래프":
                 st.info("그래프 섹션은 다음 단계에서 구현될 예정입니다.")
